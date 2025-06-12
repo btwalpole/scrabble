7ebd319b230d85ee22d7577a4e758f7b4c774d21
Ben Walpole <benwalpole95@gmail.com>
2025-06-12 16:50:57 +0100

    deriving words on the board

--- client/src/Board.tsx
+++ client/src/Board.tsx
@@ -48,16 +48,17 @@ export const Board = ({initialTiles}: {initialTiles: TileRecord[]}) => {
     const squares = renderSquares(tiles)
     const rackSquares = renderRackSquares(tiles)
 
-    const isValid = isBoardValid(tiles)
+    // const isValid = isBoardValid(tiles)
+    getRowWords(tiles)
     return (
         <>
-        {!isValid && <p>Invalid</p>}
         <div className="board">
             {squares}
         </div>
         <Rack>
             {rackSquares}
         </Rack>
+        {/* <p>{isValid ? "valid" : "invalid"}</p> */}
         </>
     )
 }
@@ -100,45 +101,74 @@ export const Board = ({initialTiles}: {initialTiles: TileRecord[]}) => {
         return squares;
     }
 
-    function isBoardValid(tiles: TileRecord[]){
+    function getRowWords(tiles: TileRecord[]){
         console.log('allTiles', tiles)
+
+        const row = 5
+        let words = []
+        let prevTile = false
+        for (let col = 0; col < 10; col++) {
+            const squareCoord: Coord = [row, col];
+            const tile = tiles.find((tile) => isEqualCoord(tile.location, squareCoord));
+            if(tile) {
+                if(words.length === 0){
+                    words.push([tile])
+                } else {
+                    if(prevTile) {
+                        //if tile is prior square, add letter to existing word
+                        words[words.length -1].push(tile)
+                    } else {
+                        //if no tile in prior square, start new word
+                        words.push([tile])
+                    }
+                }
+                prevTile = true;
+            } else {
+                prevTile = false
+            }
+
+        }
+    console.log(' words', words)
+    console.log(' wordsLetters', words.map(word => word.map(tile => tile.letter)))
+    // trim commas from start and end
+    // then do join on the commas
+    // const words = rowTiles.join(",")
+
+        return words
+    }
+
+    function isBoardValid(tiles: TileRecord[]){
+        // console.log('allTiles', tiles)
         const tilesOnTheBoard = tiles.filter(tile => tile.location[0] < 10)
         // does each distinct column and row form a valid word
             // find the rows of words
                 //check validity of each
             // find the columns of words
 
-        // at least one of the newly placed tiles must be adjacent to the initial tiles
-
-        // DONE --- - - - - - ARE ALL TILES CONNECTED
-            // check every tile for a neighbour
-            // for each tile, check the positions of all four adjcaent squares, if none have a tile in, there INVALID
-
-        // ALL NEWLY PLACED TILES MUST BE IN THE SAME ROW OR COLUMN
-            // check the fromRack tiles, do they all have either the same X or the same Y
         let invalidTiles = []
             
+        // checking that all tiles have a neighbour
         for(let i = 0; i<tilesOnTheBoard.length; i++){
             const currentTile = tilesOnTheBoard[i]
-            console.log('currentTile', currentTile)
+            // console.log('currentTile', currentTile)
             //now search through all other tiles, are any of them adjacent. if we have y=5, x=7
                 //i.e. we need y=5 (same as currTile) and x=6 or 8 i.e. currTile x+1 or x-1
                 //OR   we need y = currTile +-1  and x=same as currTile
                 const currTileLocation = currentTile.location
                 const currTileY = currTileLocation[0]
                 const currTileX = currTileLocation[1]
-            console.log('currTileY', currTileY)
-            console.log('currTileX', currTileX)
+            // console.log('currTileY', currTileY)
+            // console.log('currTileX', currTileX)
             const restOfTiles = tilesOnTheBoard.filter(tile => tile.location[0] !== currTileLocation[0] || tile.location[1] !== currTileLocation[1])
-            console.log('restOfTiles', restOfTiles)
+            // console.log('restOfTiles', restOfTiles)
 
             const neighbourFound = restOfTiles.find(tile => (tile.location[0] === currTileY && Math.abs(tile.location[1] - currTileX) <=1)
              || (tile.location[1] === currTileX && Math.abs(tile.location[0] - currTileY) <=1))
-            console.log('neighbourFound', neighbourFound)
+            // console.log('neighbourFound', neighbourFound)
             if(!neighbourFound) invalidTiles.push(currentTile)
         }
-    console.log('invalidTiles',invalidTiles)
-        return false
+    // console.log('invalidTiles',invalidTiles)
+        return invalidTiles.length === 0
     }
 
     // on first load, we fetch the placed tile coords, and the tiles in our rack
